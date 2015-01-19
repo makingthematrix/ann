115839de7300a5541a107f069a0019b151a61fea
Maciek Gorywoda <gorywoda@9livesdata.com>
2015-01-19 16:38:49 +0100

    removing the old chooseOne method

--- src/main/scala/anna/epengine/Probability.scala
+++ src/main/scala/anna/epengine/Probability.scala
@@ -23,34 +23,19 @@ object Probability{
     else Probability(1.0) :: probs.tail.map( _ => Probability(0.0) ).toList
   }
 
-  // @todo: This is rather low level. I'd prefer something returning an enum.
-  def chooseOne(probs: Probability*):Int = {
-    val list = normalize(probs: _*)
-    val r = RandomNumber()
-
-    var dSum = 0.0
-    var index = 0
-    while(dSum < r){
-      if(dSum + list(index) < r) index += 1
-      dSum += list(index)
-    }
-    index
-  }
-
-  def performRandom(tuples: (Probability,()=>Unit)*):Unit = RandomNumber() match {
+  def performRandom(tuples: (Probability,()=>Unit)*): Unit = RandomNumber() match {
     case 0.0 if tuples.map(_._1.value).sum == 0.0 => (tuples.head._2)()
     case 0.0 => (tuples.collectFirst({ case (p, f) if p > 0.0 => f }).get)()
     case r => performRandom(r, tuples.toList)
   }
 
-  private def performRandom(r: Double, tuples:List[(Probability,()=>Unit)]): Unit ={
+  private def performRandom(r: Double, tuples:List[(Probability,()=>Unit)]): Unit = {
     val list = normalize(tuples.map(_._1): _*).zip(tuples.map(_._2)).toList
     var index = 0
     var d = r
     while (d >= 0.0) {
       d -= list(index)._1
-      if (d < 0.0) list(index)._2()
-      index += 1
+      if (d < 0.0) list(index)._2() else index += 1
     }
   }
 }
