20e6cd60f11d4740ffcc68320d245b56728cf418
makingthematrix <makingthematrix@gmail.com>
2015-03-25 23:02:30 +0100

    application.conf

--- src/main/scala/anna/Context.scala
+++ src/main/scala/anna/Context.scala
@@ -1,21 +1,33 @@
 package anna
 
+import java.util.concurrent.TimeUnit
+
 import akka.actor.ActorSystem
 import akka.util.Timeout
-import anna.data.{DontForget, HushValue, SynapseWeight}
+import anna.data._
+import com.typesafe.config.ConfigFactory
 
 import scala.concurrent.duration._
 
-object Context {    
-  implicit val timeout = Timeout(5 seconds)
-  val system = ActorSystem("system")
+object Context {
+  private val config = ConfigFactory.load()
+  private val root = config.getConfig("context")
+
+  implicit val timeout = Timeout(FiniteDuration.apply(root.getInt("awaitTimeout"), TimeUnit.SECONDS))
+
+  lazy val system = ActorSystem("system")
   
   // neuron
-  val slope = 5.0
-  val threshold = 0.5
-  val weight = SynapseWeight(1.0)
-  val hushValue = HushValue()
-  val forgetting = DontForget
-  val tickTime = 30L
+  var slope = root.getDouble("defaultSlope")
+  var threshold = root.getDouble("defaultThreshold")
+  var weight = SynapseWeight(root.getDouble("defaultWeight"))
+  var hushValue = HushValue(root.getInt("defaultHushValue"))
+  var forgetting = root.getString("defaultForgetting") match {
+    case "DontForget" => DontForget
+    case "ForgetAll" => ForgetAll
+    case str => ForgetValue(str.toDouble)
+  }
+  var tickTime = root.getLong("defaultTickTime")
+
 
 }
